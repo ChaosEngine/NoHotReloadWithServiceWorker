9646a6a6890dbd60ee163c7d0704a92e99a8a684
ChaosEngine <andrzej.pauli@gmail.com>
2022-01-26 18:26:36 +0100

    iframe test

---
+++ Pages/IframeTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoHotReloadWithServiceWorker.Pages
+{
+    public class IframeTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
